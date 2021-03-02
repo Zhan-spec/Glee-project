f962de7517e84144febaee4694f5e2cd4844e874
Zhan-spec <zhan.ivan145@gmail.com>
2021-03-02 08:26:57 +0200

    added Product list 1/2 without adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,31 @@ const concat        = require('gulp-concat');
 const autoprefixer  = require('gulp-autoprefixer');
 const uglify        = require('gulp-uglify');
 const imagemin      = require('gulp-imagemin');
+const rename        = require('gulp-rename');
 const del           = require('del');
 const browserSync   = require('browser-sync').create();
 const svgSprite     = require('gulp-svg-sprite');
+// const fileinclude   = require('gulp-file-include');  // для настройки модульности с gulp-file-include
 
+
+// function includeHTML() { // для настройки модульности с gulp-file-include
+//   return src([
+//     //'./src/html/**/*.html', 
+//     //'!./src/html/parts/*.html',    //ignore
+//     //'!./src/html/pages/*.html'     //ignore
+
+
+//     './src/module/**/*.html', 
+//     '!./src/module/parts/*.html',    //ignore
+//     '!./src/module/pages/*.html'     //ignore
+//     ])
+//     .pipe(fileinclude({
+//       prefix: '@@',
+//       basepath: '@file'
+//     }))
+//     .pipe(dest('./src'))
+//     .pipe(browserSync.stream());
+// }
 
 function svgSprites() {
   return src('./src/images/icons/**.svg')
@@ -32,9 +53,14 @@ function browsersync() {
 }
 
 function styles() {
-  return src('src/scss/style.scss')
+  return src('src/scss/style.scss')  // до настройки файла index.scss   // для настройки модульности с gulp-file-include
+  //return src('src/scss/*.scss')  // после настройки файла index.scss
     .pipe(scss({outputStyle: 'compressed'}))  /* expanded | compressed */
-    .pipe(concat('style.min.css'))  /* указываем название файла после конвератции */
+    .pipe(concat('style.min.css'))  /* указываем название файла после конвератции */  // до настройки файла index.scss
+    //.pipe(concat()) // после настройки файла index.scss
+    // .pipe(rename({   // для настройки модульности с gulp-file-include
+    //   suffix : '.min'
+    // }))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 2 versions'],
       grid: true
@@ -50,6 +76,9 @@ function scripts() {
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'src/js/main.js'
   ])
   .pipe(concat('main.min.js'))
@@ -76,7 +105,8 @@ function images() {
 
 function build() {
   return src([
-    'src/**/*.html',
+    'src/**/*.html',  // строка до добавления gulp-file-include
+    //'src/html/*.html',  // строка после добавления gulp-file-include  // для настройки модульности с gulp-file-include
     'src/css/style.min.css',
     'src/js/main.min.js'
   ], {base: 'src'})
@@ -88,12 +118,21 @@ function cleanDist() {
 }
 
 function watching() {
+  watch(['src/module/**/*.scss'], styles);
   watch(['src/scss/**/*.scss'], styles);
   watch(['src/js/**/*.js', '!src/js/main.min.js'], scripts);
   /* следить за измеенниямии в 'src/js/--.js' но не следить за файлом '!src/js/main.min.js*/
 
   watch(['src/images/icons/*.svg'], svgSprites);
-  watch(['src/**/*.html']).on('change', browserSync.reload);
+
+  //watch(['src/html/**/*.html']).on('change', includeHTML);
+  //watch(['src/module/**/*.html']).on('change', includeHTML);
+  
+
+  watch(['src/**/*.html']).on('change', browserSync.reload);  // строка до добавления gulp-file-include
+  //watch(['src/**/*.html']).on('change', browserSync.reload);  // строка после добавления gulp-file-include
+
+  
 }
 
 exports.styles = styles;   /* для запуска function styles() */
@@ -102,6 +141,7 @@ exports.svgSprites = svgSprites;
 exports.browsersync = browsersync;
 exports.watching = watching;  /* для запуска function watching */
 exports.images = images;
+//exports.includeHTML = includeHTML;
 //exports.build = build;   /*  если запускаем build сразу для удаления dist, сжатия картинок и копирования файлов из src в dist то используем build с series*/
 
 
@@ -110,3 +150,6 @@ exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
 exports.default = parallel(styles, scripts, svgSprites, browsersync, watching);
+
+// для настройки модульности с gulp-file-include
+//exports.default = parallel(styles, includeHTML, scripts, svgSprites, browsersync, watching);
--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,61 @@ $(function() {
   //         }
   // });
 
+  $('.pagination__item').on('click', function() {
+    $('.pagination__item').removeClass('pagination__item--active');
+    $(this).addClass('pagination__item--active');
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.products__item').removeClass('products__item--list');
+    $('.product-card').removeClass('product-card--list');    
+  });
+
+  $('.button-list').on('click', function () {
+    $('.products__item').addClass('products__item--list');
+    $('.product-card').addClass('product-card--list');    
+  });
+
+  $('.product-content__filter-btn').on('click',function () {
+    $('.product-content__filter-btn').removeClass('product-content__filter-btn--active');
+    $(this).addClass('product-content__filter-btn--active');
+  });
+
+  $('.product-content__select').styler();
+
+
+  $('.product-content__filter-btn').on('click', function(e) {
+    e.preventDefault();
+
+    $('.product-content__filter-btn').removeClass('product-content__filter-btn--active');
+    $(this).addClass('product-content__filter-btn--active');
+
+  });
+
+
+
+  $(".star").rateYo({
+    starWidth: "16px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true
+  });
+
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",        
+    prefix: "$",
+    step: 0.01,
+    prettify_enabled: true,
+    prettify_separator: ".",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    }
+  });
 
   $('.footer__title--nav').on('click', function() {
     $(this).siblings().slideToggle();
@@ -74,6 +129,8 @@ $(function() {
     ]
   });
   
+
+  // для mixitup писать в кончце файла
   var containerEl1 = document.querySelector('[data-ref="products__mixitup"]');
   var containerEl2 = document.querySelector('[data-ref="design__mixitup"]');
 
